9a003c941062c50b0f709dc718dd1984466bef7b
Endre Karlson <endre.karlson@gmail.com>
2013-08-26 01:02:03 +0200

    Add a index for now

--- src/js/opendaylight.js
+++ src/js/opendaylight.js
@@ -2,8 +2,23 @@
 
 // Make sure to include the `ui.router` module as a dependency
 angular.module('opendaylight', ['ui.router']).run(
-  [        '$rootScope', '$state', '$stateParams',
+  ['$rootScope', '$state', '$stateParams',
   function ($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
+}]);
+
+
+angular.module('opendaylight').config(
+  ['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+    $stateProvider.state('index', {
+        url: '/',
+        templateUrl: 'index.html'
+    });
+    $stateProvider.state('help', {
+        url: '/help',
+    });
 }]);
