e32b8938df195e74ec753a0f1923a275b77f7b77
Endre Karlson <endre.karlson@gmail.com>
2013-08-26 03:39:58 +0200

    unused for now

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+var proto = 'http'
+var host = 'localhost';
+var port = '8080';
+var path = '/v1'
+
+angular.element(document).ready(function() {
+        opendaylight.value('config', {
+                endpoint : proto + '://' + host + '\\:' + port + path
+        })
+        angular.bootstrap(document, ['opendaylight']);
+});
