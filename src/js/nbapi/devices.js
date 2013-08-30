58a288308044adff41d21798610d8b22ba5a8938
Endre Karlson <endre.karlson@gmail.com>
2013-08-30 22:33:30 +0200

    Fixup the devices view and add more details

--- src/js/nbapi/devices.js
+++ src/js/nbapi/devices.js
@@ -8,12 +8,30 @@ opendaylight.factory('NodeSvc', ['Restangular', function (Restangular) {
         return Restangular.all('switch/' + (container || 'default') + '/nodes').getList();
     }
 
+    svc.getNode = function(container, type, id) {
+        return Restangular.one('switch', (container || 'default')).one('node', type).one(id).get()
+    }
+
     return svc
 }]);
 
 opendaylight.controller('DevicesCtrl', ['$scope', 'NodeSvc', function($scope, NodeSvc) {
-    NodeSvc.getNodes().then(function(data) {
-        $scope.properties = data;
+    $scope.ncpData = {}
+
+    // Fetch the nodes then fetch more info about each node
+    NodeSvc.getNodes().then(function(npData) {
+        $scope.npData = npData.nodeProperties;
+
+        angular.forEach(npData.nodeProperties, function (np) {
+            NodeSvc.getNode('default', np.node['@type'], np.node['@id']).then(
+                function(ncp, test) {
+                    $scope.ncpData[np.node['@id']] = ncp.nodeConnectorProperties
+                }
+            )
+
+
+        })
+        //$scope.properties = data;
     });
 }]);
 
